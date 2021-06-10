ec8a625ad401ff75421ad2df1dca98991f7f6896
Bruno <bruno.umbelino0@gmail.com>
2021-06-10 00:16:24 -0300

    adding small appearance enhancement to the Form component

--- src/Components/Form/FormElements.js
+++ src/Components/Form/FormElements.js
@@ -3,9 +3,10 @@ import { Formik, Form, Field } from "formik";
 
 export const FormWrap = styled.div`
   padding: 2rem;
-  border: 1px dotted white;
+  ///border: 1px solid white;
   display: flex;
   flex-direction: column;
+  width: 400px;
 `;
 
 export const FormikWrap = styled(Formik)``;
@@ -26,7 +27,7 @@ export const FormikField = styled(Field)`
 export const FormButton = styled.button`
   width: 100%;
   height: 100%;
-  border-radius: 2rem;
+  border-radius: 8px;
 `;
 
 export const RedirectWrap = styled.div`
